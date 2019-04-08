cd0d6c7ff9cdc113b071b76330adc9b4ee4b8a8a
gabrielvf1 <vf_gabriel@yahoo.com.br>
2019-04-08 15:42:43 -0300

    integracao tela led e rtt

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -1,18 +1,133 @@
-/*
+/*2pir*pulsos
  * main.c
  *
  * Created: 05/03/2019 18:00:58
  *  Author: eduardo
  */ 
 
-#include <asf.h>
+#include "asf.h"
 #include "tfont.h"
 #include "sourcecodepro_28.h"
 #include "calibri_36.h"
 #include "arial_72.h"
 
+/************************************************************************/
+/* defines                                                              */
+/************************************************************************/
+
+#define LED_PIO       PIOC
+#define LED_PIO_ID    ID_PIOC
+#define LED_IDX       8u
+#define LED_IDX_MASK  (1u << LED_IDX)
+
+#define BUT1_PIO      PIOD
+#define BUT1_PIO_ID   ID_PIOD
+#define BUT1_IDX  28
+#define BUT1_IDX_MASK (1 << BUT1_IDX)
 
 struct ili9488_opt_t g_ili9488_display_opt;
+
+/************************************************************************/
+/* constants                                                            */
+/************************************************************************/
+
+/************************************************************************/
+/* variaveis globais                                                    */
+/************************************************************************/
+volatile Bool f_rtt_alarme = false;
+volatile int contador_roda;
+volatile Bool flag_aumentou = false;
+
+/************************************************************************/
+/* prototypes                                                           */
+/************************************************************************/
+void pin_toggle(Pio *pio, uint32_t mask);
+void io_init(void);
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses);
+
+/************************************************************************/
+/* interrupcoes                                                         */
+/************************************************************************/
+
+void RTT_Handler(void)
+{
+	uint32_t ul_status;
+
+	/* Get RTT status */
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Time has changed */
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {  }
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		pin_toggle(LED_PIO, LED_IDX_MASK);    // BLINK Led
+		f_rtt_alarme = true;                  // flag RTT alarme
+	}
+}
+
+/************************************************************************/
+/* funcoes                                                              */
+/************************************************************************/
+void but1_callBack(void){
+	pio_set(LED_PIO,LED_IDX_MASK);
+	contador_roda += 1;
+	flag_aumentou = true;
+}
+
+void pin_toggle(Pio *pio, uint32_t mask){
+	if(pio_get_output_data_status(pio, mask))
+	pio_clear(pio, mask);
+	else
+	pio_set(pio,mask);
+}
+
+void io_init(void){
+	/* led */
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT);
+	
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_IDX_MASK, PIO_PULLUP);
+	
+	pio_handler_set(BUT1_PIO,
+	BUT1_PIO_ID,
+	BUT1_IDX_MASK,
+	PIO_IT_FALL_EDGE,
+	but1_callBack);
+	
+	pio_enable_interrupt(BUT1_PIO, BUT1_IDX_MASK);
+	
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 4);
+	
+}
+
+static float get_time_rtt(){
+	uint ul_previous_time = rtt_read_timer_value(RTT);
+}
+
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
+{
+	uint32_t ul_previous_time;
+
+	/* Configure RTT for a 1 second tick interrupt */
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	
+	ul_previous_time = rtt_read_timer_value(RTT);
+	while (ul_previous_time == rtt_read_timer_value(RTT));
+	
+	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+
+	/* Enable RTT interrupt */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 0);
+	NVIC_EnableIRQ(RTT_IRQn);
+	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN);
+}
+
 
 void configure_lcd(void){
 	/* Initialize display parameter */
@@ -44,14 +159,54 @@ void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
 
 
 int main(void) {
+	WDT->WDT_MR = WDT_MR_WDDIS;
 	board_init();
 	sysclk_init();	
+	io_init();
 	configure_lcd();
+	 f_rtt_alarme = true;
 	
 	font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
 	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
 	font_draw_text(&arial_72, "102456", 50, 200, 2);
 	while(1) {
-		
-	}
+		 if (f_rtt_alarme){
+      
+      /*
+       * O clock base do RTT é 32678Hz
+       * Para gerar outra base de tempo é necessário
+       * usar o PLL pre scale, que divide o clock base.
+       *
+       * Nesse exemplo, estamos operando com um clock base
+       * de pllPreScale = 32768/32768/2 = 2Hz
+       *
+       * Quanto maior a frequência maior a resolução, porém
+       * menor o tempo máximo que conseguimos contar.
+       *
+       * Podemos configurar uma IRQ para acontecer quando 
+       * o contador do RTT atingir um determinado valor
+       * aqui usamos o irqRTTvalue para isso.
+       * 
+       * Nesse exemplo o irqRTTvalue = 8, causando uma
+       * interrupção a cada 2 segundos (lembre que usamos o 
+       * pllPreScale, cada incremento do RTT leva 500ms (2Hz).
+       */
+      uint16_t pllPreScale = (int) (((float) 32768) / 2.0); //500ms
+      uint32_t irqRTTvalue  = 4;
+      
+      // reinicia RTT para gerar um novo IRQ
+      RTT_init(pllPreScale, irqRTTvalue);         
+      
+     /*
+      * caso queira ler o valor atual do RTT, basta usar a funcao
+      *   rtt_read_timer_value()
+      */
+      
+      /*
+       * CLEAR FLAG
+       */
+      f_rtt_alarme = false;
+    }
+  }  
+	return 0;
 }
